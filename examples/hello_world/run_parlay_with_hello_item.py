f30c495b29a604deaa51411c961a63415a465fd2
Jeff Gable <jeff@promenadesoftware.com>
2016-02-17 14:29:47 -0800

    hello_world: make say_hello message a property

--- examples/hello_world/run_parlay_with_hello_item.py
+++ examples/hello_world/run_parlay_with_hello_item.py
@@ -1,12 +1,14 @@
-from parlay import start, local_item, parlay_command, ParlayCommandItem
+from parlay import start, local_item, parlay_command, ParlayCommandItem, parlay_property
 
 
 @local_item(auto_connect=True)
 class CheerfulPerson(ParlayCommandItem):
 
+    hello_string = parlay_property(default="Hello World!", val_type=str)
+
     @parlay_command()
     def say_hello(self):
-        return "Hello World!"
+        return self.hello_string
 
 
 if __name__ == "__main__":
