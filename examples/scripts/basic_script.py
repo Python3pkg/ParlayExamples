8803b3ccb84ab909410b657cef2f1277bb4ff1be
Daniel Beard <daniel@promenadesoftware.com>
2016-05-10 16:53:10 -0700

    PARLAY-1 : example of using datastream and waiting on it

--- examples/scripts/basic_script.py
+++ examples/scripts/basic_script.py
@@ -3,4 +3,17 @@ from parlay.utils import setup, discover, get_item_by_name
 setup()
 discover()
 e = get_item_by_name("Adder")
-print e.add(2,3)
+e.x = 10
+e.y = 12
+handle = e.send_parlay_command("add", x=1, y=2)
+handle.wait_for_ack()
+print "ack1"
+handle.wait_for_ack()
+print "ack2"
+
+st = e.get_datastream_handle("st")
+print st.get()
+e.send_parlay_command("poll_stream")
+
+while st.get() < 100:
+    print st.wait_for_value()
